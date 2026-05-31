5de73a29df9a39a24b7d11e1f559799ff18c964e
snorreks <snorrekstrand@hotmail.com>
2026-05-31 19:31:00 +0200

    chore: update aikami imports

--- apps/frontend/pwa/src/routes/(authenticated)/personas/create/+page.svelte
+++ apps/frontend/pwa/src/routes/(authenticated)/personas/create/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { page } from '$app/state';
-  import PersonaCreationView from '$views/persona/create/PersonaCreationView.svelte';
-  import { getPersonaCreationViewModel } from '$views/persona/create/persona-creation-view-model.svelte.ts';
+  import PersonaCreationView from '$views/persona/create/persona_creation_view.svelte';
+  import { getPersonaCreationViewModel } from '$views/persona/create/persona_creation_view_model.svelte';
 
   const isOnboarding = page.url.searchParams.get('onboarding') === 'true';
 
--- packages/frontend/services/src/index.ts
+++ packages/frontend/services/src/index.ts
@@ -10,7 +10,7 @@ export * from './lib/firebase/firebase_analytics_service.ts';
 export * from './lib/firebase/firebase_auth_service.ts';
 export * from './lib/firebase/firebase_cloud_messaging.ts';
 export * from './lib/firebase/firebase_functions_service.ts';
-export * from './lib/firebase/firebase-remote_config.ts';
+export * from './lib/firebase/firebase_remote_config.ts';
 export * from './lib/firebase/firebase_storage.ts';
 export * from './lib/router/router_utils.ts';
 export * from './lib/services/dialog.svelte.ts';
